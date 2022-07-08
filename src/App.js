4107ef89818e5fe6565e0896c51a9f03c30a4b20
dgl <mykulych15@gmail.com>
2022-07-08 16:15:04 +0300

    feat: created routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
+
 function App() {
-  return (
-    <div className="container mx-auto bg-dark">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <div>{useRoutes(routes)}</div>;
 }
 
 export default App;
