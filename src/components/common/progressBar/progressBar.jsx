a41697f4a0dc541ae706fc3afb2183b8860a9ccb
dgl <mykulych15@gmail.com>
2022-07-10 22:19:15 +0300

    fix: fixed user page layout

--- src/components/common/progressBar/progressBar.jsx
+++ src/components/common/progressBar/progressBar.jsx
@@ -5,7 +5,7 @@ function ProgressBar({ color, completed, label }) {
   return (
     <>
       <div className="font-semibold">{label}</div>
-      <div className="w-1/4 bg-gray-200 rounded-full h-5 ">
+      <div className="w-full bg-gray-200 rounded-full h-5 ">
         <div
           style={{ width: `${completed}%` }}
           className={` ${color} text-s font-medium text-blue-100 text-center p-0.5 leading-none rounded-lg h-5  `}
